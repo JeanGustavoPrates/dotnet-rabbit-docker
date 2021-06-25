02029db00d4290d020602a1b9fc81b686bed09ae
Jean Gustavo Prates <jean.prates@terceiroslinx.com.br>
2021-06-25 15:30:35 -0300

    first commit

---
+++ src/ConsumerRabbitMq/Program.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit;
+using Newtonsoft.Json;
+using PublisherRabbitMq;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsumerRabbitMq
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri("rabbitmq://rabbit"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint("value-events-listener", e =>
+                {
+                    e.Consumer<ValueConsumer>();
+                });
+            });
+
+            await bus.StartAsync();
+            Console.WriteLine("Waiting for messages...");
+            Console.ReadLine();
+
+            await bus.StopAsync();
+        }
+
+        public class ValueConsumer : IConsumer<Message>
+        {
+            public Task Consume(ConsumeContext<Message> context)
+            {
+                Console.WriteLine($"Received [x] {JsonConvert.SerializeObject(context.Message)} [at] {DateTime.Now:u}");
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ src/PublisherRabbitMq/Program.cs
@@ -0,0 +1,38 @@
+﻿using MassTransit;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PublisherRabbitMq
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host(new Uri("rabbitmq://rabbit"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+
+            await bus.StartAsync();
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.WhenAll(Enumerable.Range(0, 100).Select(i =>
+                {
+                    var message = new Message { Value = $"Message {i}" };
+                    return bus.Publish(message);
+                }));
+
+                Thread.Sleep(10000);
+            }
+
+            await bus.StopAsync();
+        }
+    }
+}
